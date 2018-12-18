6aa3cd24fd4d08651e6bcff70e49446b5b76e993
Liu Jingqi <jingqi.liu@intel.com>
2018-12-18 03:46:33 -0500

    x86/cpu: Enable MOVDIRI cpu feature
    MOVDIRI moves doubleword or quadword from register to memory through
    direct store which is implemented by using write combining (WC) for
    writing data directly into memory without caching the data.
    
    The bit definition:
    CPUID.(EAX=7,ECX=0):ECX[bit 27] MOVDIRI
    
    The release document ref below link:
    https://software.intel.com/sites/default/files/managed/c5/15/\
    architecture-instruction-set-extensions-programming-reference.pdf
    
    Backports commit 24261de4916596d8ab5f5fee67e9e7a19e8325a5 from qemu

--- qemu/target/i386/cpu.c
+++ qemu/target/i386/cpu.c
@@ -863,7 +863,7 @@ static FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             "avx512bitalg", NULL, "avx512-vpopcntdq", NULL,
             "la57", NULL, NULL, NULL,
             NULL, NULL, "rdpid", NULL,
-            NULL, "cldemote", NULL, NULL,
+            NULL, "cldemote", NULL, "movdiri",
             NULL, NULL, NULL, NULL,
         },
         .cpuid = {
