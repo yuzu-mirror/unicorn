60ccaf56ac53aa18e3ea45a46309ea5e190ee4a4
Peter Maydell <peter.maydell@linaro.org>
2019-02-03 17:36:00 -0500

    target/arm/translate-a64: Don't underdecode SIMD ld/st multiple
    In the AdvSIMD load/store multiple structures encodings,
    the non-post-indexed case should have zeroes in [20:16]
    (which is the Rm field for the post-indexed case).
    Correctly UNDEF the currently unallocated encodings which
    have non-zeroes in those bits.
    
    Backports commit e1f220811dbd5d85fb02ff286358f9ee6188938f from qemu

--- qemu/target/arm/translate-a64.c
+++ qemu/target/arm/translate-a64.c
@@ -3322,6 +3322,7 @@ static void disas_ldst_multiple_struct(DisasContext *s, uint32_t insn)
     TCGContext *tcg_ctx = s->uc->tcg_ctx;
     int rt = extract32(insn, 0, 5);
     int rn = extract32(insn, 5, 5);
+    int rm = extract32(insn, 16, 5);
     int size = extract32(insn, 10, 2);
     int opcode = extract32(insn, 12, 4);
     bool is_store = !extract32(insn, 22, 1);
@@ -3337,6 +3338,11 @@ static void disas_ldst_multiple_struct(DisasContext *s, uint32_t insn)
     int r;
 
     if (extract32(insn, 31, 1) || extract32(insn, 21, 1)) {
+        unallocated_encoding(s);
+        return;
+    }
+
+    if (!is_postidx && rm != 0) {
         unallocated_encoding(s);
         return;
     }
@@ -3440,7 +3446,6 @@ static void disas_ldst_multiple_struct(DisasContext *s, uint32_t insn)
     }
 
     if (is_postidx) {
-        int rm = extract32(insn, 16, 5);
         if (rm == 31) {
             tcg_gen_mov_i64(tcg_ctx, tcg_rn, tcg_addr);
         } else {
