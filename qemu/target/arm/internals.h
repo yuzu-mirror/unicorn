8124b9f9757fbe28186430d990d8a2ed289816b1
Peter Maydell <peter.maydell@linaro.org>
2019-02-05 17:43:00 -0500

    target/arm: Compute TB_FLAGS for TBI for user-only
    Enables, but does not turn on, TBI for CONFIG_USER_ONLY.
    
    Backports commit c47eaf9fc2af68cfbdbd9ae31f8e2e5ebb7022b4 from qemu

--- qemu/target/arm/internals.h
+++ qemu/target/arm/internals.h
@@ -965,31 +965,9 @@ typedef struct ARMVAParameters {
     bool using64k   : 1;
 } ARMVAParameters;
 
-#ifdef CONFIG_USER_ONLY
-static inline ARMVAParameters aa64_va_parameters_both(CPUARMState *env,
-                                                      uint64_t va,
-                                                      ARMMMUIdx mmu_idx)
-{
-    ARMVAParameters result = {0};
-    /* 48-bit address space */
-    result.tsz = 16;
-    /* We can't handle tagged addresses properly in user-only mode */
-    result.tbi = false;
-
-    return result;
-}
-
-static inline ARMVAParameters aa64_va_parameters(CPUARMState *env,
-                                                 uint64_t va,
-                                                 ARMMMUIdx mmu_idx, bool data)
-{
-    return aa64_va_parameters_both(env, va, mmu_idx);
-}
-#else
 ARMVAParameters aa64_va_parameters_both(CPUARMState *env, uint64_t va,
                                         ARMMMUIdx mmu_idx);
 ARMVAParameters aa64_va_parameters(CPUARMState *env, uint64_t va,
                                    ARMMMUIdx mmu_idx, bool data);
-#endif
 
 #endif
