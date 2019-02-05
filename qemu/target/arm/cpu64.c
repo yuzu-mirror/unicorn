fbd8992e27f6e00d8caf77a7e14cbb6a3a76e3b8
Richard Henderson <richard.henderson@linaro.org>
2019-02-05 17:15:28 -0500

    target/arm: Enable BTI for -cpu max
    Backports commit a15daafa1cba96ff28abdfb6c860e0939655dbd1 from qemu

--- qemu/target/arm/cpu64.c
+++ qemu/target/arm/cpu64.c
@@ -269,6 +269,10 @@ static void aarch64_max_initfn(struct uc_struct *uc, Object *obj, void *opaque)
     t = FIELD_DP64(t, ID_AA64PFR0, ADVSIMD, 1);
     cpu->isar.id_aa64pfr0 = t;
 
+    t = cpu->isar.id_aa64pfr1;
+    t = FIELD_DP64(t, ID_AA64PFR1, BT, 1);
+    cpu->isar.id_aa64pfr1 = t;
+
     t = cpu->isar.id_aa64mmfr1;
     t = FIELD_DP64(t, ID_AA64MMFR1, HPDS, 1); /* HPD */
     t = FIELD_DP64(t, ID_AA64MMFR1, LO, 1);
