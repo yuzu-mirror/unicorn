190ee1657be82cdbdd6ba98b4b773da8348e63f0
Richard Henderson <richard.henderson@linaro.org>
2019-03-13 11:18:25 -0400

    decodetree: Add --static-decode option
    Like --decode, but do not drop 'static' qualifier.
    
    Backports commit cd3e7fc18db43b296f413814cd4b72bcd6878bc4 from qemu

--- qemu/scripts/decodetree.py
+++ qemu/scripts/decodetree.py
@@ -1115,7 +1115,8 @@ def main():
 
     decode_scope = 'static '
 
-    long_opts = ['decode=', 'translate=', 'output=', 'insnwidth=']
+    long_opts = ['decode=', 'translate=', 'output=', 'insnwidth=',
+                 'static-decode=']
     try:
         (opts, args) = getopt.getopt(sys.argv[1:], 'o:w:', long_opts)
     except getopt.GetoptError as err:
@@ -1126,6 +1127,8 @@ def main():
         elif o == '--decode':
             decode_function = a
             decode_scope = ''
+        elif o == '--static-decode':
+            decode_function = a
         elif o == '--translate':
             translate_prefix = a
             translate_scope = ''
