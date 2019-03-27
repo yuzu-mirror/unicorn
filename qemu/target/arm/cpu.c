f5cb1a5865c2bde1de5634b3d68b3fc6a18b8184
Richard Henderson <richard.henderson@linaro.org>
2019-03-26 20:40:51 -0400

    target/arm: Set SIMDMISC and FPMISC for 32-bit -cpu max
    Fixes: https://bugs.launchpad.net/bugs/1821430
    
    Backports commit c8877d0f2f662bf01346a03bc9fd279954b4132d from qemu

--- qemu/target/arm/cpu.c
+++ qemu/target/arm/cpu.c
@@ -1718,6 +1718,11 @@ static void arm_max_initfn(struct uc_struct *uc, Object *obj, void *opaque)
         t = FIELD_DP32(t, ID_ISAR6, SPECRES, 1);
         cpu->isar.id_isar6 = t;
 
+        t = cpu->isar.mvfr2;
+        t = FIELD_DP32(t, MVFR2, SIMDMISC, 3); /* SIMD MaxNum */
+        t = FIELD_DP32(t, MVFR2, FPMISC, 4);   /* FP MaxNum */
+        cpu->isar.mvfr2 = t;
+
         t = cpu->id_mmfr4;
         t = FIELD_DP32(t, ID_MMFR4, HPDS, 1); /* AA32HPD */
         cpu->id_mmfr4 = t;
