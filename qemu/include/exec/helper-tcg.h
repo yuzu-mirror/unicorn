70836028eb91ccabf1bd66e52063df19ee7ee76a
Lioncash <mathew1800@gmail.com>
2019-04-22 08:20:09 -0400

    exec/helper-*: Synchronize with qemu

--- qemu/include/exec/helper-tcg.h
+++ qemu/include/exec/helper-tcg.h
@@ -11,37 +11,44 @@
 #define str(s) #s
 
 #define DEF_HELPER_FLAGS_0(NAME, FLAGS, ret) \
-  { HELPER(NAME), str(#NAME), FLAGS | dh_callflag(ret), \
-    dh_sizemask(ret, 0) },
+  { .func = HELPER(NAME), .name = str(NAME), \
+    .flags = FLAGS | dh_callflag(ret), \
+    .sizemask = dh_sizemask(ret, 0) },
 
 #define DEF_HELPER_FLAGS_1(NAME, FLAGS, ret, t1) \
-  { HELPER(NAME), str(#NAME), FLAGS | dh_callflag(ret), \
-    dh_sizemask(ret, 0) | dh_sizemask(t1, 1) },
+  { .func = HELPER(NAME), .name = str(NAME), \
+    .flags = FLAGS | dh_callflag(ret), \
+    .sizemask = dh_sizemask(ret, 0) | dh_sizemask(t1, 1) },
 
 #define DEF_HELPER_FLAGS_2(NAME, FLAGS, ret, t1, t2) \
-  { HELPER(NAME), str(#NAME), FLAGS | dh_callflag(ret), \
-    dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
+  { .func = HELPER(NAME), .name = str(NAME), \
+    .flags = FLAGS | dh_callflag(ret), \
+    .sizemask = dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
     | dh_sizemask(t2, 2) },
 
 #define DEF_HELPER_FLAGS_3(NAME, FLAGS, ret, t1, t2, t3) \
-  { HELPER(NAME), str(#NAME), FLAGS | dh_callflag(ret), \
-    dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
+  { .func = HELPER(NAME), .name = str(NAME), \
+    .flags = FLAGS | dh_callflag(ret), \
+    .sizemask = dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
     | dh_sizemask(t2, 2) | dh_sizemask(t3, 3) },
 
 #define DEF_HELPER_FLAGS_4(NAME, FLAGS, ret, t1, t2, t3, t4) \
-  { HELPER(NAME), str(#NAME), FLAGS | dh_callflag(ret), \
-    dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
+  { .func = HELPER(NAME), .name = str(NAME), \
+    .flags = FLAGS | dh_callflag(ret), \
+    .sizemask = dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
     | dh_sizemask(t2, 2) | dh_sizemask(t3, 3) | dh_sizemask(t4, 4) },
 
 #define DEF_HELPER_FLAGS_5(NAME, FLAGS, ret, t1, t2, t3, t4, t5) \
-  { HELPER(NAME), str(#NAME), FLAGS | dh_callflag(ret), \
-    dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
+  { .func = HELPER(NAME), .name = str(NAME), \
+    .flags = FLAGS | dh_callflag(ret), \
+    .sizemask = dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
     | dh_sizemask(t2, 2) | dh_sizemask(t3, 3) | dh_sizemask(t4, 4) \
     | dh_sizemask(t5, 5) },
 
 #define DEF_HELPER_FLAGS_6(NAME, FLAGS, ret, t1, t2, t3, t4, t5, t6) \
-  { HELPER(NAME), str(NAME), FLAGS | dh_callflag(ret), \
-    dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
+  { .func = HELPER(NAME), .name = str(NAME), \
+    .flags = FLAGS | dh_callflag(ret), \
+    .sizemask = dh_sizemask(ret, 0) | dh_sizemask(t1, 1) \
     | dh_sizemask(t2, 2) | dh_sizemask(t3, 3) | dh_sizemask(t4, 4) \
     | dh_sizemask(t5, 5) | dh_sizemask(t6, 6) },
 
