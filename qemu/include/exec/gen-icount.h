830756a725fc8049f2c4e2864f5514575378c132
Lioncash <mathew1800@gmail.com>
2019-04-22 06:17:09 -0400

    gen-icount: Use tcg_ctx where applicable in commented out code
    If this is ever used in the future, it'll already be able to be used.

--- qemu/include/exec/gen-icount.h
+++ qemu/include/exec/gen-icount.h
@@ -22,34 +22,34 @@ static inline void gen_tb_start(TCGContext *tcg_ctx, TranslationBlock *tb)
 #if 0
     tcg_ctx->exitreq_label = gen_new_label();
     if (tb_cflags(tb) & CF_USE_ICOUNT) {
-        count = tcg_temp_local_new_i32();
+        count = tcg_temp_local_new_i32(tcg_ctx);
     } else {
-        count = tcg_temp_new_i32();
+        count = tcg_temp_new_i32(tcg_ctx);
     }
 
-    tcg_gen_ld_i32(count, cpu_env,
+    tcg_gen_ld_i32(tcg_ctx, count, tcg_ctx->cpu_env,
                    -ENV_OFFSET + offsetof(CPUState, icount_decr.u32));
 
     if (tb_cflags(tb) & CF_USE_ICOUNT) {
-        imm = tcg_temp_new_i32();
+        imm = tcg_temp_new_i32(tcg_ctx);
         /* We emit a movi with a dummy immediate argument. Keep the insn index
          * of the movi so that we later (when we know the actual insn count)
          * can update the immediate argument with the actual insn count.  */
-        tcg_gen_movi_i32(imm, 0xdeadbeef);
-        icount_start_insn = tcg_last_op();
+        tcg_gen_movi_i32(tcg_ctx, imm, 0xdeadbeef);
+        icount_start_insn = tcg_last_op(tcg_ctx);
 
-        tcg_gen_sub_i32(count, count, imm);
-        tcg_temp_free_i32(imm);
+        tcg_gen_sub_i32(tcg_ctx, count, count, imm);
+        tcg_temp_free_i32(tcg_ctx, imm);
     }
 
-    tcg_gen_brcondi_i32(TCG_COND_LT, count, 0, tcg_ctx->exitreq_label);
+    tcg_gen_brcondi_i32(tcg_ctx, TCG_COND_LT, count, 0, tcg_ctx->exitreq_label);
 
     if (tb_cflags(tb) & CF_USE_ICOUNT) {
-        tcg_gen_st16_i32(count, cpu_env,
+        tcg_gen_st16_i32(tcg_ctx, count, tcg_ctx, cpu_env,
                          -ENV_OFFSET + offsetof(CPUState, icount_decr.u16.low));
     }
 
-    tcg_temp_free_i32(count);
+    tcg_temp_free_i32(tcg_ctx, count);
 #endif
 }
 
@@ -59,7 +59,7 @@ static inline void gen_tb_end(TCGContext *tcg_ctx, TranslationBlock *tb, int num
     if (tb_cflags(tb) & CF_USE_ICOUNT) {
         /* Update the num_insn immediate parameter now that we know
          * the actual insn count.  */
-        tcg_set_insn_param(icount_start_insn, 1, num_insns);
+        tcg_set_insn_param(tcg_ctx, icount_start_insn, 1, num_insns);
     }
 #endif
 
