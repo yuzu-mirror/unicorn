9f0e4691428e1c9b09da0840d7c3098043b0dc25
Lioncash <mathew1800@gmail.com>
2019-04-22 05:52:41 -0400

    gen-icount: Synchronize with qemu

--- qemu/include/exec/gen-icount.h
+++ qemu/include/exec/gen-icount.h
@@ -6,12 +6,10 @@
 /* Helpers for instruction counting code generation.  */
 
 //static TCGOp *icount_start_insn;
-//static TCGLabel *icount_label;
-//static TCGLabel *exitreq_label;
 
 static inline void gen_tb_start(TCGContext *tcg_ctx, TranslationBlock *tb)
 {
-    //TCGv_i32 count, flag, imm;
+    //TCGv_i32 count, imm;
     TCGv_i32 flag;
 
     tcg_ctx->exitreq_label = gen_new_label(tcg_ctx);
@@ -22,45 +20,51 @@ static inline void gen_tb_start(TCGContext *tcg_ctx, TranslationBlock *tb)
     tcg_temp_free_i32(tcg_ctx, flag);
 
 #if 0
-    if (!(tb->cflags & CF_USE_ICOUNT))
-        return;
+    tcg_ctx->exitreq_label = gen_new_label();
+    if (tb_cflags(tb) & CF_USE_ICOUNT) {
+        count = tcg_temp_local_new_i32();
+    } else {
+        count = tcg_temp_new_i32();
     }
 
-    icount_label = gen_new_label(tcg_ctx);
-    count = tcg_temp_local_new_i32(tcg_ctx);
-    tcg_gen_ld_i32(tcg_ctx, count, tcg_ctx->tcg_env,
+    tcg_gen_ld_i32(count, cpu_env,
                    -ENV_OFFSET + offsetof(CPUState, icount_decr.u32));
-    imm = tcg_temp_new_i32(tcg_ctx);
-    /* We emit a movi with a dummy immediate argument. Keep the insn index
-     * of the movi so that we later (when we know the actual insn count)
-     * can update the immediate argument with the actual insn count.  */
-    icount_start_insn = tcg_last_op(tcg_ctx);
-    tcg_gen_movi_i32(tcg_ctx, imm, 0xdeadbeef);
 
-    tcg_gen_sub_i32(tcg_ctx, count, count, imm);
-    tcg_temp_free_i32(tcg_ctx, imm);
+    if (tb_cflags(tb) & CF_USE_ICOUNT) {
+        imm = tcg_temp_new_i32();
+        /* We emit a movi with a dummy immediate argument. Keep the insn index
+         * of the movi so that we later (when we know the actual insn count)
+         * can update the immediate argument with the actual insn count.  */
+        tcg_gen_movi_i32(imm, 0xdeadbeef);
+        icount_start_insn = tcg_last_op();
 
-    tcg_gen_brcondi_i32(tcg_ctx, TCG_COND_LT, count, 0, icount_label);
-    tcg_gen_st16_i32(tcg_ctx, count, tcg_ctx->tcg_env,
-                     -ENV_OFFSET + offsetof(CPUState, icount_decr.u16.low));
-    tcg_temp_free_i32(tcg_ctx, count);
+        tcg_gen_sub_i32(count, count, imm);
+        tcg_temp_free_i32(imm);
+    }
+
+    tcg_gen_brcondi_i32(TCG_COND_LT, count, 0, tcg_ctx->exitreq_label);
+
+    if (tb_cflags(tb) & CF_USE_ICOUNT) {
+        tcg_gen_st16_i32(count, cpu_env,
+                         -ENV_OFFSET + offsetof(CPUState, icount_decr.u16.low));
+    }
+
+    tcg_temp_free_i32(count);
 #endif
 }
 
 static inline void gen_tb_end(TCGContext *tcg_ctx, TranslationBlock *tb, int num_insns)
 {
-    gen_set_label(tcg_ctx, tcg_ctx->exitreq_label);
-    tcg_gen_exit_tb(tcg_ctx, tb, TB_EXIT_REQUESTED);
-
 #if 0
-    if (tb->cflags & CF_USE_ICOUNT) {
+    if (tb_cflags(tb) & CF_USE_ICOUNT) {
         /* Update the num_insn immediate parameter now that we know
          * the actual insn count.  */
-        tcg_set_insn_param(tcg_ctx->icount_start_insn, 1, num_insns);
-        gen_set_label(tcg_ctx, icount_label);
-        tcg_gen_exit_tb(tcg_ctx, (uintptr_t)tb + TB_EXIT_ICOUNT_EXPIRED);
+        tcg_set_insn_param(icount_start_insn, 1, num_insns);
     }
 #endif
+
+    gen_set_label(tcg_ctx, tcg_ctx->exitreq_label);
+    tcg_gen_exit_tb(tcg_ctx, tb, TB_EXIT_REQUESTED);
 }
 
 #if 0
