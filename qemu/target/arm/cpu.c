1d97d223c337178ce5de5864901791042234c140
Clement Deschamps <clement.deschamps@greensocs.com>
2020-03-21 12:15:58 -0400

    target/arm: add PMU feature to cortex-r5 and cortex-r5f
    The PMU is not optional on cortex-r5 and cortex-r5f (see
    the "Features" chapter of the Technical Reference Manual).
    
    Backports commit 90f671581ac601fcc1b840d9e9abe7e3c3e672db from qemu

--- qemu/target/arm/cpu.c
+++ qemu/target/arm/cpu.c
@@ -1332,6 +1332,7 @@ static void cortex_r5_initfn(struct uc_struct *uc, Object *obj, void *opaque)
     set_feature(&cpu->env, ARM_FEATURE_V7);
     set_feature(&cpu->env, ARM_FEATURE_V7MP);
     set_feature(&cpu->env, ARM_FEATURE_PMSA);
+    set_feature(&cpu->env, ARM_FEATURE_PMU);
     cpu->midr = 0x411fc153; /* r1p3 */
     cpu->id_pfr0 = 0x0131;
     cpu->id_pfr1 = 0x001;
