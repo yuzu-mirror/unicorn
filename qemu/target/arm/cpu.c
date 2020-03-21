fd6c635e038b65bab1570136ee802327aaec4f9e
Peter Maydell <peter.maydell@linaro.org>
2020-03-21 18:16:53 -0400

    target/arm: Add and use FIELD definitions for ID_AA64DFR0_EL1
    Add FIELD() definitions for the ID_AA64DFR0_EL1 and use them
    where we currently have hard-coded bit values.
    
    Backports commit ceb2744b47a1ef4184dca56a158eb3156b6eba36 from qemu

--- qemu/target/arm/cpu.c
+++ qemu/target/arm/cpu.c
@@ -942,7 +942,7 @@ static int arm_cpu_realizefn(struct uc_struct *uc, DeviceState *dev, Error **err
         arm_register_pre_el_change_hook(cpu, &pmu_pre_el_change, 0);
         arm_register_el_change_hook(cpu, &pmu_post_el_change, 0);
     } else {
-        cpu->id_aa64dfr0 &= ~0xf00;
+        cpu->id_aa64dfr0 = FIELD_DP64(cpu->id_aa64dfr0, ID_AA64DFR0, PMUVER, 0);
         cpu->id_dfr0 &= ~(0xf << 24);
         cpu->pmceid0 = 0;
         cpu->pmceid1 = 0;
