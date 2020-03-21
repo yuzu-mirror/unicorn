764207865abf1ca25fbab424a7b3e3dcb3a6ae97
Richard Henderson <richard.henderson@linaro.org>
2020-03-21 19:45:04 -0400

    target/arm: Set ID_MMFR4.HPDS for aarch64_max_initfn
    We had set this for aarch32-only in arm_max_initfn, but
    failed to set the same bit for aarch64.
    
    Backports commit dac65ba1d7945c5d58ab63d8769103634adb2b01 from qemu

--- qemu/target/arm/cpu64.c
+++ qemu/target/arm/cpu64.c
@@ -348,6 +348,7 @@ static void aarch64_max_initfn(struct uc_struct *uc, Object *obj, void *opaque)
     cpu->isar.id_mmfr3 = u;
 
     u = cpu->isar.id_mmfr4;
+    u = FIELD_DP32(u, ID_MMFR4, HPDS, 1); /* AA32HPD */
     u = FIELD_DP32(u, ID_MMFR4, AC2, 1); /* ACTLR2, HACTLR2 */
     cpu->isar.id_mmfr4 = u;
 
