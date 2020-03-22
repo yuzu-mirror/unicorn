ab7c1570d8943cccdae593f6962a42578d93b25d
Alistair Francis <alistair.francis@wdc.com>
2020-03-22 01:16:43 -0400

    target/riscv: Add the force HS exception mode
    Add a FORCE_HS_EXCEP mode to the RISC-V virtulisation status. This bit
    specifies if an exeption should be taken to HS mode no matter the
    current delegation status. This is used when an exeption must be taken
    to HS mode, such as when handling interrupts.
    
    Backports commit c7b1bbc80fc2af17395d3986c346fd2307e57829 from qemu

--- qemu/target/riscv/cpu.h
+++ qemu/target/riscv/cpu.h
@@ -276,6 +276,8 @@ int riscv_cpu_gdb_write_register(CPUState *cpu, uint8_t *buf, int reg);
 bool riscv_cpu_exec_interrupt(CPUState *cs, int interrupt_request);
 bool riscv_cpu_virt_enabled(CPURISCVState *env);
 void riscv_cpu_set_virt_enabled(CPURISCVState *env, bool enable);
+bool riscv_cpu_force_hs_excep_enabled(CPURISCVState *env);
+void riscv_cpu_set_force_hs_excep(CPURISCVState *env, bool enable);
 int riscv_cpu_mmu_index(CPURISCVState *env, bool ifetch);
 hwaddr riscv_cpu_get_phys_page_debug(CPUState *cpu, vaddr addr);
 void  riscv_cpu_do_unaligned_access(CPUState *cs, vaddr addr,
