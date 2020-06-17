4731a69d6668e71ae28127c31dcea60b2d60ad37
Peter Maydell <peter.maydell@linaro.org>
2020-06-17 00:28:54 -0400

    target/arm: Convert Neon VEXT to decodetree
    Convert the Neon VEXT insn to decodetree. Rather than keeping the
    old implementation which used fixed temporaries cpu_V0 and cpu_V1
    and did the extraction with by-hand shift and logic ops, we use
    the TCG extract2 insn.
    
    We don't need to special case 0 or 8 immediates any more as the
    optimizer is smart enough to throw away the dead code.
    
    Backports commit 0aad761fb0aed40c99039eacac470cbd03d07019 from qemu

--- qemu/target/arm/translate-neon.inc.c
+++ qemu/target/arm/translate-neon.inc.c
@@ -2845,3 +2845,81 @@ static bool trans_VQDMLSL_2sc(DisasContext *s, arg_2scalar *a)
 
     return do_2scalar_long(s, a, opfn[a->size], accfn[a->size]);
 }
+
+static bool trans_VEXT(DisasContext *s, arg_VEXT *a)
+{
+    TCGContext *tcg_ctx = s->uc->tcg_ctx;
+
+    if (!arm_dc_feature(s, ARM_FEATURE_NEON)) {
+        return false;
+    }
+
+    /* UNDEF accesses to D16-D31 if they don't exist. */
+    if (!dc_isar_feature(aa32_simd_r32, s) &&
+        ((a->vd | a->vn | a->vm) & 0x10)) {
+        return false;
+    }
+
+    if ((a->vn | a->vm | a->vd) & a->q) {
+        return false;
+    }
+
+    if (a->imm > 7 && !a->q) {
+        return false;
+    }
+
+    if (!vfp_access_check(s)) {
+        return true;
+    }
+
+    if (!a->q) {
+        /* Extract 64 bits from <Vm:Vn> */
+        TCGv_i64 left, right, dest;
+
+        left = tcg_temp_new_i64(tcg_ctx);
+        right = tcg_temp_new_i64(tcg_ctx);
+        dest = tcg_temp_new_i64(tcg_ctx);
+
+        neon_load_reg64(s, right, a->vn);
+        neon_load_reg64(s, left, a->vm);
+        tcg_gen_extract2_i64(tcg_ctx, dest, right, left, a->imm * 8);
+        neon_store_reg64(s, dest, a->vd);
+
+        tcg_temp_free_i64(tcg_ctx, left);
+        tcg_temp_free_i64(tcg_ctx, right);
+        tcg_temp_free_i64(tcg_ctx, dest);
+    } else {
+        /* Extract 128 bits from <Vm+1:Vm:Vn+1:Vn> */
+        TCGv_i64 left, middle, right, destleft, destright;
+
+        left = tcg_temp_new_i64(tcg_ctx);
+        middle = tcg_temp_new_i64(tcg_ctx);
+        right = tcg_temp_new_i64(tcg_ctx);
+        destleft = tcg_temp_new_i64(tcg_ctx);
+        destright = tcg_temp_new_i64(tcg_ctx);
+
+        if (a->imm < 8) {
+            neon_load_reg64(s, right, a->vn);
+            neon_load_reg64(s, middle, a->vn + 1);
+            tcg_gen_extract2_i64(tcg_ctx, destright, right, middle, a->imm * 8);
+            neon_load_reg64(s, left, a->vm);
+            tcg_gen_extract2_i64(tcg_ctx, destleft, middle, left, a->imm * 8);
+        } else {
+            neon_load_reg64(s, right, a->vn + 1);
+            neon_load_reg64(s, middle, a->vm);
+            tcg_gen_extract2_i64(tcg_ctx, destright, right, middle, (a->imm - 8) * 8);
+            neon_load_reg64(s, left, a->vm + 1);
+            tcg_gen_extract2_i64(tcg_ctx, destleft, middle, left, (a->imm - 8) * 8);
+        }
+
+        neon_store_reg64(s, destright, a->vd);
+        neon_store_reg64(s, destleft, a->vd + 1);
+
+        tcg_temp_free_i64(tcg_ctx, destright);
+        tcg_temp_free_i64(tcg_ctx, destleft);
+        tcg_temp_free_i64(tcg_ctx, right);
+        tcg_temp_free_i64(tcg_ctx, middle);
+        tcg_temp_free_i64(tcg_ctx, left);
+    }
+    return true;
+}
