2f0940677e7cb15ee5a763a8cf12b2ecf77df5db
Peter Maydell <peter.maydell@linaro.org>
2021-03-01 20:36:01 -0500

    target/arm: Implement FPSCR.LTPSIZE for M-profile LOB extension
    If the M-profile low-overhead-branch extension is implemented, FPSCR
    bits [18:16] are a new field LTPSIZE. If MVE is not implemented
    (currently always true for us) then this field always reads as 4 and
    ignores writes.
    
    These bits used to be the vector-length field for the old
    short-vector extension, so we need to take care that they are not
    misinterpreted as setting vec_len. We do this with a rearrangement
    of the vfp_set_fpscr() code that deals with vec_len, vec_stride
    and also the QC bit; this obviates the need for the M-profile
    only masking step that we used to have at the start of the function.
    
    We provide a new field in CPUState for LTPSIZE, even though this
    will always be 4, in preparation for MVE, so we don't have to
    come back later and split it out of the vfp.xregs[FPSCR] value.
    (This state struct field will be saved and restored as part of
    the FPSCR value via the vmstate_fpscr in machine.c.)
    
    Backports 8128c8e8cc9489a8387c74075974f86dc0222e7f

--- qemu/target/arm/cpu.c
+++ qemu/target/arm/cpu.c
@@ -251,6 +251,15 @@ static void arm_cpu_reset(CPUState *s)
         uint32_t initial_pc; /* Loaded from 0x4 */
         uint32_t vecbase = 0;
 
+        if (cpu_isar_feature(aa32_lob, cpu)) {
+            /*
+             * LTPSIZE is constant 4 if MVE not implemented, and resets
+             * to an UNKNOWN value if MVE is implemented. We choose to
+             * always reset to 4.
+             */
+            env->v7m.ltpsize = 4;
+        }
+
         if (arm_feature(env, ARM_FEATURE_M_SECURITY)) {
             env->v7m.secure = true;
         } else {
